79f76d8fa22acbe26a0a58e7a0633aeffa3ae083
Tiago Montes <tiago.montes@gmail.com>
2019-12-28 03:50:18 +0000

    Refactored Sprite classes removing code duplication.

--- src/aturtle/sprite/bitmap.py
+++ src/aturtle/sprite/bitmap.py
@@ -5,7 +5,6 @@
 # See LICENSE for details.
 # ----------------------------------------------------------------------------
 
-import math
 from . import base
 
 
@@ -13,14 +12,7 @@ class BitmapSprite(base.BaseSprite):
 
     def __init__(self, canvas, image, *, x=0, y=0):
 
-        self._canvas = canvas
-        self._id = None
-
-        self._x_anchor = x
-        self._y_anchor = y
-        self._theta = 0
-
-        self._image = image
+        super().__init__(canvas, image, x=x, y=y)
 
         self._id = self._canvas.create_image(
             x - image.cx,
@@ -30,80 +22,21 @@ class BitmapSprite(base.BaseSprite):
         )
 
 
-    @staticmethod
-    def _anchor_offset(length, offset):
-
-        return length * offset if isinstance(offset, float) else offset
-
-
-    @property
-    def x(self):
-
-        return self._x_anchor
-
-
-    @property
-    def y(self):
-
-        return self._y_anchor
-
-
-    def move(self, dx=0, dy=0, *, update=False):
-
-        self._x_anchor += dx
-        self._y_anchor += dy
-        self._canvas.move(self._id, dx, dy)
-        if update:
-            self.update()
-
-
-    def moveto(self, x=0, y=0, *, update=False):
-
-        self.move(x - self._x_anchor, y - self._y_anchor, update=update)
-
-
     def rotate(self, theta=0, *, around=None, update=False):
 
-        self._theta = (self._theta + theta) % (math.pi * 2)
+        # Rotate anchor point if needed.
+        super().rotate(theta, around=around, update=False)
+
+        # Anchor point rotated, move the image.
+        if around:
+            self._canvas.moveto(
+                self._id,
+                self._x_anchor - self._image.cx,
+                self._y_anchor - self._image.cy,
+            )
 
         # Use the pre-rendered image for the new orientation.
         self._canvas.itemconfig(self._id, image=self._image[self._theta])
 
-        # Rotate anchor point, if not rotating around it.
-        if around:
-            cx, cy = around
-            x = self._x_anchor - cx
-            y = self._y_anchor - cy
-            cos_theta = math.cos(theta)
-            sin_theta = math.sin(theta)
-            new_x = x * cos_theta - y * sin_theta + cx
-            new_y = x * sin_theta + y * cos_theta + cy
-            self._x_anchor = new_x
-            self._y_anchor = new_y
-            self._canvas.moveto(
-                self._id,
-                new_x - self._image.cx,
-                new_y - self._image.cy,
-            )
-
         if update:
             self.update()
-
-
-    def unrotate(self, update=False):
-
-        self.rotate(-self._theta, update=update)
-
-
-    def update(self):
-
-        # TODO: Use update_idletasks, instead?
-        #       http://www.tcl.tk/man/tcl8.6/TclCmd/update.htm
-        self._canvas.update()
-
-
-    def delete(self):
-
-        if self._id:
-            self._canvas.delete(self._id)
-            self._id = None
