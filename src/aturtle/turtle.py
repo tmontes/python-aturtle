0f678649d81879aed8be45568455fe1e540abce9
Tiago Montes <tiago.montes@gmail.com>
2020-01-16 14:27:06 +0000

    Turtle drawing flag renamed to down + up()/down() toggle methods.

--- src/aturtle/turtle.py
+++ src/aturtle/turtle.py
@@ -9,22 +9,33 @@
 
 class Turtle:
 
-    def __init__(self, sprite, *, drawing=True):
+    def __init__(self, sprite, *, down=True):
 
         self._canvas = sprite.canvas
         self._sprite = sprite
 
-        self._drawing = drawing
+        self._down = down
 
         self._line_id = None
         self._line_start = None
         self._lines = []
 
 
-    def _draw_line(self, _progress, anchor):
+    def up(self):
+
+        self._down = False
+
+
+    def down(self):
+
+        self._down = True
+
+
+    def _sync_draw_line(self, _progress, anchor):
 
         if self._line_id:
             self._canvas.coords(self._line_id, *self._line_start, *anchor)
+            # TODO: Improve this, cannot access private sprite attribute.
             self._canvas.tag_lower(self._line_id, self._sprite._id)
         else:
             self._line_id = self._canvas.create_line(*self._line_start, *anchor)
@@ -37,5 +48,5 @@ class Turtle:
         self._line_start = self._sprite.anchor
         self._sprite.sync_forward(
             distance,
-            callback=self._draw_line if self._drawing else None,
+            callback=self._sync_draw_line if self._down else None,
         )
