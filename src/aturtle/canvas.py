de46bdbc2517dcba13c9115111a46dda0d2b06c8
Tiago Montes <tiago.montes@gmail.com>
2020-01-22 19:18:10 +0000

    InvertedYTkCanvas wraps tkinter.Canvas instead of inheriting from it.

--- src/aturtle/canvas.py
+++ src/aturtle/canvas.py
@@ -9,15 +9,58 @@ import tkinter
 
 
 
-class InvertedYTkCanvas(tkinter.Canvas):
+class InvertedYTkCanvas:
 
     def __init__(self, master, background):
 
-        super().__init__(
+        self._canvas = tkinter.Canvas(
             master,
             highlightthickness=0,
             background=background,
         )
+
+    def pack(self, *, expand, fill):
+
+        return self._canvas.pack(expand=expand, fill=fill)
+
+
+    def config(self, *, xscrollincrement, yscrollincrement):
+
+        return self._canvas.config(
+            xscrollincrement=xscrollincrement,
+            yscrollincrement=yscrollincrement,
+        )
+
+
+    def xview_scroll(self, number, what):
+
+        return self._canvas.xview_scroll(number, what)
+
+
+    def yview_scroll(self, number, what):
+
+        return self._canvas.yview_scroll(number, what)
+
+
+
+    def itemconfig(self, item_id, *, image):
+
+        return self._canvas.itemconfig(item_id, image=image)
+
+
+    def update(self):
+
+        return self._canvas.update()
+
+
+    def tag_raise(self, item_id, other=None):
+
+        return self._canvas.tag_raise(item_id, other)
+
+
+    def tag_lower(self, item_id, other=None):
+
+        return self._canvas.tag_lower(item_id, other)
 
 
     def _inverted_y(self, coords):
@@ -30,7 +73,7 @@ class InvertedYTkCanvas(tkinter.Canvas):
 
     def create_polygon(self, coords, *, fill, outline, width):
 
-        return super().create_polygon(
+        return self._canvas.create_polygon(
             self._inverted_y(coords),
             fill=fill,
             outline=outline,
@@ -40,17 +83,12 @@ class InvertedYTkCanvas(tkinter.Canvas):
 
     def create_image(self, x, y, *, image, anchor):
 
-        return super().create_image(x, -y, image=image, anchor=anchor)
-
-
-    def move(self, item_id, dx, dy):
-
-        return super().move(item_id, dx, -dy)
+        return self._canvas.create_image(x, -y, image=image, anchor=anchor)
 
 
     def create_line(self, coords, *, fill, width, capstyle):
 
-        return super().create_line(
+        return self._canvas.create_line(
             self._inverted_y(coords),
             fill=fill,
             width=width,
@@ -58,7 +96,16 @@ class InvertedYTkCanvas(tkinter.Canvas):
         )
 
 
+    def move(self, item_id, dx, dy):
+
+        return self._canvas.move(item_id, dx, -dy)
+
+
     def coords(self, item_id, coords):
 
-        return super().coords(item_id, self._inverted_y(coords))
+        return self._canvas.coords(item_id, self._inverted_y(coords))
 
+
+    def delete(self, item_id):
+
+        return self._canvas.delete(item_id)
